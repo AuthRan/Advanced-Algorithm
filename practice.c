3a1d71ad52f88a1ced38481a5000e08c7886d75b
Ashutosh Ranjan <authran.off@gmail.com>
2025-12-01 09:00:27 +0530

    Done revision of VCP with 2 approximation

--- practice.c
+++ practice.c
@@ -2,22 +2,40 @@
 #include <stdlib.h>
 #include <string.h>
 
-void computeLPSArray(char *pat, int m, int *lps) {
-    int i = 1;
-    int length = 0;
-    lps[0] = 0;
-    while(i<m) {
-        if(lps[i] == lps[length]){
-            length++;
-            lps[i] = length;
-            i++;
-        }else{
-            if(length != 0){
-                length = lps[length-1];
-            }else{
-                lps[i] = 0;
-                i++;
+typedef struct {
+    int u;
+    int v;
+    int used;
+}Edges;
+
+int main() {
+    int n = 10;
+    int m = 24;
+    Edges edges[m];
+    for(int i=0; i<m; i++) {
+        scanf("%d", &edges[i].u);
+        scanf("%d", &edges[i].v);
+        edges[i].used = 0;
+    }
+    int cover[100];
+    int k = 0;
+    for(int i=0; i<m; i++) {
+        if(!edges[i].used) {
+            int u = edges[i].u;
+            int v = edges[i].v;
+            cover[k++] = u;
+            cover[k++] = v;
+            edges[i].used = 1;
+            for(int j=0; j<m; j++) {
+                if(!edges[j].used) {
+                    if(edges[j].u == u || edges[j].u == v || edges[j].v == u || edges[j].v == v){
+                        edges[j].used = 1;
+                    }
+                }
             }
         }
+    }
+    for(int i=0; i<k; i++) {
+        printf("%d ", cover[i]);
     }
 }
